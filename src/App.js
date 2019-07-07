87a2174e778a9a38b2c24aed1200f064e9f18562
abdo hassan <abdocoder0@gmail.com>
2019-07-07 17:05:18 +0200

    styling

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Dashboard from './components/dashboard/Dashboard';
 import ProjectDetails from './components/projects/ProjectDetails';
+import SignIn from './components/auth/SignIn';
+import signUp from './components/auth/SignUp';
+import CreateProject from './components/projects/CreateProject';
 
 function App() {
   return (
@@ -12,6 +15,9 @@ function App() {
         <Switch>
           <Route exact path='/' component={Dashboard} />
           <Route path='/project/:id' component={ProjectDetails} />
+          <Route path='/signin' component={SignIn} />
+          <Route path='/signup' component={signUp} />
+          <Route path='/create' component={CreateProject} />
         </Switch>
       </div>
     </BrowserRouter>

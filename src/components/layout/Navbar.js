ec21f3c146bfcc24251bfefe719f4fe081bec48c
abdo hassan <abdocoder0@gmail.com>
2019-07-17 11:49:08 +0200

    set up firebase functions + hosting

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -16,7 +16,7 @@ const Navbar = props => {
     <nav className='nav wrapper grey darken-3'>
       <div className='container'>
         <Link to='/' className='brand-logo'>
-          Main Project
+          Project Manager
         </Link>
         {links}
       </div>
